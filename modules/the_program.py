f37e266a19a30b97609ec54478bcbafa53310d0d
Vedant Patadia <v.mr.gamer@gmail.com>
2021-08-24 13:04:00 -0400

    Documenting

--- modules/the_program.py
+++ modules/the_program.py
@@ -1,3 +1,21 @@
+## @package modules
+#  the_program class to automate the result generation
+# Adapted from TensorFlow Tutorial, Convolutional Variational Autoencoders
+# https://github.com/tensorflow/docs/blob/master/site/en/tutorials/generative/cvae.ipynb
+#
+# LICENSE
+# @title Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from CVAE import CVAE
 from dataset_loader import dataset_loader
 from model_helper import model_helper
@@ -12,11 +30,13 @@ import glob
 import matplotlib.pyplot as plt
 
 class the_program:
+    ## constructor
     def __init__(self, dataset='mnist', latent_dimen=2):
         self.data = dataset_loader('mnist')
         self.model = CVAE(latent_dimen, self.data.input_shape)
         self.m_helper = model_helper(self.model, self.data)
 
+    ## function to generate and save activations
     def generate_and_save_images(self, epoch):
         mean, logvar = self.model.encode(self.m_helper.test_sample)
         z = self.model.reparameterize(mean, logvar)
@@ -32,6 +52,7 @@ class the_program:
         plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
         plt.show()
     
+    ## function for automating tasks 
     def run(self):
         self.generate_and_save_images(0)
 
